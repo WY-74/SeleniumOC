e51869a2c8989ab25b0000a58b7feda0b97583cc
Wang.Y <294961525@qq.com>
2023-11-06 20:01:30 +0800

    231106: add get_attrs

--- selenium_oc.py
+++ selenium_oc.py
@@ -6,8 +6,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from typing import List
 
-class SeleniumOC():
+
+class SeleniumOC:
     # Unified use of CSS SELECTOR for locators
+    # Get Chromedriver at https://googlechromelabs.github.io/chrome-for-testing/
 
     def __init__(self, driver):
         self.driver = driver
@@ -20,7 +22,19 @@ class SeleniumOC():
             if _text == by_text:
                 return elements[i]
         return None
-    
+
+    def _get_text_with_multiple(self, css_selector, multiple: bool = True):
+        if multiple:
+            elems = self.get_elements(css_selector, multiple=True)
+            return [elem.text for elem in elems]
+        return self.get_elements(css_selector).text
+
+    def _get_attr_with_multiple(self, css_selector, attr, multiple: bool = True):
+        if multiple:
+            elems = self.get_elements(css_selector, multiple=True)
+            return [elem.get_attribute(attr) for elem in elems]
+        return self.get_elements(css_selector).get_attribute(attr)
+
     def _scroll_and_input(self, css_selector: str, send_keys: str):
         element = self.get_elements(css_selector)
         self.scroll_to_element(element)
@@ -41,6 +55,9 @@ class SeleniumOC():
     def get_url(self):
         return self.driver.current_url
 
+    def get_handle_title(self):
+        return self.driver.title
+
     def scroll_to_element(self, element: WebElement):
         location_y = element.location['y'] - 130
         location_y = 0 if location_y < 0 else location_y
@@ -52,39 +69,57 @@ class SeleniumOC():
         if by_text:
             return self._get_element_by_text(css_selector, by_text)
         return self.driver.find_element(By.CSS_SELECTOR, css_selector)
-    
+
     def must_get_element(self, css_selector: str, time_out: int = 30):
         # Wait for an element (default wait time is 30s) if the timeout is exceeded, the program terminates.
 
         return WebDriverWait(self.driver, timeout=time_out).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)),
-            message=f"[E] expected element not found: {css_selector}"
+            message=f"[E] expected element not found: {css_selector}",
         )
-    
+
     def must_get_text(self, css_selector: str, text: str, time_out: int = 30):
-        # Wait for the  text of the element (default wait time is 30 seconds), if the timeout is exceeded, the program will terminate.
-            
+        # Wait for the text of the element (default wait time is 30 seconds), if the timeout is exceeded, the program will terminate.
+
         return WebDriverWait(self.driver, timeout=time_out).until(
             EC.text_to_be_present_in_element((By.CSS_SELECTOR, css_selector), text),
-            message=f"[E] expected text not found: {text}"
+            message=f"[E] expected text not found: {text}",
         )
-    
+
     def must_no_element(self, css_selector: str, time_out: int = 30):
         return WebDriverWait(self.driver, timeout=time_out).until(
             EC.invisibility_of_element_located((By.CSS_SELECTOR, css_selector)),
-            message=f"[E] expected elements remain: {css_selector}"
+            message=f"[E] expected elements remain: {css_selector}",
         )
-    
-    def get_text(self, css_selector: str|List[str], multiple: bool = False):
-        if isinstance(css_selector, str):
-            return self.get_elements(css_selector).text
+
+    def get_text(self, css_selector: str | List[str], multiple: bool = False):
+        # Allow passing single or multiple css selectors
+
+        # The multiple defaults to False, which means that we will target the first matching element for each css selector
+        # If set the multiple to True it will match all matching elements of each css selector.
+
         if isinstance(css_selector, list):
             result = []
             for i in css_selector:
-                _text = self.get_elements(i).text
+                _text = self._get_text_with_multiple(i, multiple)
                 result.append(_text) if _text else result.append("")
             return result
-    
+        return self._get_text_with_multiple(css_selector, multiple)
+
+    def get_attrs(self, css_selector: str | List[str], attr: str, multiple: bool = False):
+        # Allow passing single or multiple css selectors
+
+        # The multiple defaults to False, which means that we will target the first matching element for each css selector
+        # If set the multiple to True it will match all matching elements of each css selector.
+
+        if isinstance(css_selector, list):
+            result = []
+            for i in css_selector:
+                _attr = self._get_attr_with_multiple(i, attr, multiple)
+                result.append(_attr) if _attr else result.append("")
+            return result
+        return self._get_attr_with_multiple(css_selector, attr, multiple)
+
     def scroll_and_click(self, css_selector: str, by_text: str = ""):
         element = self.get_elements(css_selector, by_text=by_text)
         if not element:
@@ -96,7 +131,7 @@ class SeleniumOC():
         element.click()
         return True
 
-    def scroll_and_input(self, css_selector: str|List[str], send_keys: str|List[str]):
+    def scroll_and_input(self, css_selector: str | List[str], send_keys: str | List[str]):
         if isinstance(css_selector, str) and isinstance(send_keys, str):
             self._scroll_and_input(css_selector, send_keys)
             return True
@@ -105,7 +140,7 @@ class SeleniumOC():
                 self._scroll_and_input(css_selector[i], send_keys[i])
             return True
         return False
-    
+
     def switch_window_and_execute(self, handle_index, fun, back: bool = True, **kwargs):
         # 1. Switch windows
         # 2. Execute function
@@ -113,12 +148,12 @@ class SeleniumOC():
 
         handles = self.driver.window_handles
         self.driver.switch_to.window(handles[handle_index])
-        fun(**kwargs)
+        result = fun(**kwargs)
         if back:
             self.driver.switch_to.window(handles[handle_index])
+        return result
 
     def save_html_to_local(self, pth: str):
         _html = self.driver.page_source
         with open(pth, "w", encoding="utf-8") as f:
             f.write(_html)
-
